d3b719e078bf1b8b9adc2f5db73f0713549f2b66
apurvasr <112015458+apurvasr@users.noreply.github.com>
2022-11-08 17:47:44 -0500

    Song Page created calling itunes search page

---
+++ MovieSongSearch/MovieSongSearch/Pages/Song.cshtml.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using static System.Net.WebRequestMethods;
+using SongDatabase;
+
+namespace MovieSongSearch.Pages
+{
+    public class SongModel : PageModel
+    {
+        private readonly ILogger<SongModel> _logger;
+        static readonly HttpClient client = new HttpClient();
+        public bool SearchCompleted { get; set; }
+        public string Query { get; set; }
+        public Song songs;
+        public SongModel(ILogger<SongModel> logger)
+        {
+            _logger = logger;
+        }
+        public async Task OnGetAsync(string query)
+        {
+            Query = query;
+            if (!string.IsNullOrEmpty(query))
+            {
+                SearchCompleted = true;
+                String s1 = "https://itunes.apple.com/search?term=";
+                String f = s1 + query + "&resultCount=5";
+                var task = client.GetAsync(f);
+                HttpResponseMessage response = task.Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    Task<string> readString = response.Content.ReadAsStringAsync();
+
+                    string jsonString = readString.Result;
+
+                    songs = Song.FromJson(jsonString);
+                }
+
+            }
+            else
+            {
+                SearchCompleted = false;
+            }
+
+        }
+    }
+}
